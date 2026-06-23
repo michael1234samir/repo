1ded5b67dbf6b91fdcb12cc70f406d14be837d13
michael1234samir <michael1234samir@gmail.com>
2026-06-23 19:22:43 +0300

    Add files via upload

---
+++ Protofolio.js
@@ -0,0 +1,7 @@
+const typed = new Typed("#text", {
+  strings: ["   Frontend Dev", "  Backend Dev", "  Fullstack Dev"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+const text = document.getElementById("text");
