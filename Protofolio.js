167ab83279544e0bdd3bfbfdaf0f3e86d7b2d502
michael1234samir <michael1234samir@gmail.com>
2026-06-27 10:37:08 +0300

    Add navbar toggle functionality to Protofolio.js

--- Protofolio.js
+++ Protofolio.js
@@ -4,4 +4,29 @@ const typed = new Typed("#text", {
   backSpeed: 100,
   loop: true,
 });
+const togglebtn = document.getElementById("toggle-btn");
+const navbar = document.getElementById("navbar");
+if (window.screen.width >= 700) {
+  navbar.classList.add("close");
+}
+function togglenavbar() {
+  navbar.classList.toggle("close");
+  togglebtn.classList.toggle("rotate");
+  closeallsidebar();
+}
 const text = document.getElementById("text");
+function togglesubmenu(button) {
+  if (!button.nextElementSibling.classList.contains("show")) closeallsidebar();
+  button.nextElementSibling.classList.toggle("show");
+  button.classList.toggle("rotate");
+  if (navbar.classList.contains("close")) {
+    navbar.classList.toggle("close");
+    togglebtn.classList.toggle("rotate");
+  }
+}
+function closeallsidebar() {
+  Array.from(navbar.getElementsByClassName("show")).forEach((el) => {
+    el.classList.remove("show");
+    el.nextElementSibling.remove("rotate");
+  });
+}
